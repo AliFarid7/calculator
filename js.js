2d7ffa69bc9262b8ab034405813e650c04376502
Ali Farid <54527194+AliFarid7@users.noreply.github.com>
2026-02-12 23:07:36 +0200

    Add first iteration of JS

--- js.js
+++ js.js
@@ -24,6 +24,48 @@ function operator(op, num1, num2){
     if (op === "*") return multiply(num1, num2);
     if (op === "/") return divide(num1, num2);
 }
+const digitButtons = document.querySelectorAll(".digit");
+const operatorButtons = document.querySelectorAll(".operator");
+let myDisplay = document.querySelector(".display");
+let myResult = document.querySelector(".result");
 
+function handleNumber(){
+    digitButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        myDisplay.textContent += button.textContent;
+    });
+});
+}
 
-console.log(operator('*', 5, 6));
+function handleOperator(){
+    operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        num1 = Number(myDisplay.textContent);
+        myDisplay.textContent += button.textContent;
+        op = button.textContent;
+            operatorButtons.forEach(button => {
+            button.disabled = true;
+            });        
+        myDisplay.textContent = "";
+    });
+});
+}
+
+function getResult(){
+    myResult.addEventListener("click", () =>{
+        if (num1 === undefined || op === undefined) return;
+        num2 = Number(myDisplay.textContent);
+        const result = operator(op, num1, num2);
+        myDisplay.textContent = result;
+        num1 = result;
+        op = undefined;
+        num2 = undefined;
+        operatorButtons.forEach(button => {
+    button.disabled = false;
+        });
+    })
+}
+
+handleNumber();
+handleOperator();
+getResult();
